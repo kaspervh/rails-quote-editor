8eacd482ae4b59fcb288b4aacf7eb5c18c18632d
kaspervh <kaspervhauschildt@protonmail.com>
2025-08-01 14:55:31 +0200

    made initial tests

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -1,6 +1,10 @@
 require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
+  setup do
+    @quote = quotes(:first)
+  end
+
   test "Creating quote " do 
     visit quote_path
     assert_selector 'h1', text: 'Quote'
@@ -14,9 +18,34 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector 'h1', text: 'quotes'
     assert_text 'this is a test'
   end  
-  # test "visiting the index" do
-  #   visit quotes_url
-  #
-  #   assert_selector "h1", text: "Quotes"
-  # end
+
+  test "showing a quote" do 
+    visit quote_path
+    click_link @quote.name
+    assert_selector 'h1', text: @quote.name
+
+  end
+
+  test "Updating a quote" do 
+    visit quotes_path
+    assert_selector 'h1', text: "Quotes"
+
+    click_on 'edit'
+    assert_selector 'h1', text: 'edit quote'
+
+    fill_in 'Name', with: 'updated quote'
+    click_on 'update quote'
+
+    assert_selector 'h1', text: 'Quotes'
+    assert_text 'updated quote'
+  end
+
+  test 'destroying a quote' do
+    visit quotes_path
+    assert_selector 'h1', text: 'Quotes'
+
+    click_on 'delete', match: :first
+    assert_no_text @quote.name
+  end
+ 
 end
