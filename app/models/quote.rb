74e2df62014d552f0863a231e42d5d311943cc5a
kaspervh <kaspervhauschildt@protonmail.com>
2025-08-18 15:56:42 +0200

    added turbo stream o delete and update

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,7 @@
 class Quote < ApplicationRecord
   validates :name, presence: :true
 
-  after_create_commit -> { broadcast_prepend_to "quotes"}
+  after_create_commit -> { broadcast_prepend_to "quotes" }
+  after_update_commit -> { broadcast_replace_to "quotes" }
+  after_destroy_commit -> { broadcast_destroy_to "quotes" }
 end
