63203ecf7ab4857c2e0dedf9d4124330b45218d4
kaspervh <kaspervhauschildt@protonmail.com>
2025-08-20 16:20:46 +0200

    made a shitty but functional navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  before_acion :authenticate_user!, unless: :devise_controller?
+  before_action :authenticate_user!, unless: :devise_controller?
 end
